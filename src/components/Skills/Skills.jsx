dd5665a14800e1f4acb17b0046f504db2ad17bb8
PROFESSOR <92365820+X-PROFESSOR@users.noreply.github.com>
2022-11-05 23:13:52 +0530

    dev3

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -13,7 +13,48 @@ function Skills() {
           ispum is simpley dummy text of printing</p>
       </div>
       <div className="s-right">
-        Its right
+        <div className="s-bars">
+          <div className="bar">
+            <div className="info">
+              <span>HTML</span>
+            </div>
+            <div className="progress-line html">
+              <span></span>
+            </div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>CSS</span>
+            </div>
+            <div className="progress-line css">
+              <span></span>
+            </div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>jQuery</span>
+            </div>
+            <div className="progress-line jquery">
+              <span></span>
+            </div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>Python</span>
+            </div>
+            <div className="progress-line python">
+              <span></span>
+            </div>
+          </div>
+          <div className="bar">
+            <div className="info">
+              <span>MySQL</span>
+            </div>
+            <div className="progress-line mysql">
+              <span></span>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
