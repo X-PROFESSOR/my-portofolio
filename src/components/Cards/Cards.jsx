4dc1353d833343cae1053c6bff716344f302db02
PROFESSOR <92365820+X-PROFESSOR@users.noreply.github.com>
2022-11-06 01:50:28 +0530

    dev4

---
+++ src/components/Cards/Cards.jsx
@@ -0,0 +1,44 @@
+import React from 'react'
+import './Cards.css'
+
+const Cards = ({header, l1, l2, l3, l4}) => {
+    return (
+        <div className="s-bars">
+            <p className="card-head">{header}</p>
+            <div className="bar">
+                <div className="info">
+                    <span>{l1}</span>
+                </div>
+                <div className={`progress-line ${l1}`}>
+                    <span></span>
+                </div>
+            </div>
+            <div className="bar">
+                <div className="info">
+                    <span>{l2}</span>
+                </div>
+                <div className={`progress-line ${l2}`}>
+                    <span></span>
+                </div>
+            </div>
+            <div className="bar">
+                <div className="info">
+                    <span>{l3}</span>
+                </div>
+                <div className={`progress-line ${l3}`}>
+                    <span></span>
+                </div>
+            </div>
+            <div className="bar">
+                <div className="info">
+                    <span>{l4}</span>
+                </div>
+                <div className={`progress-line ${l4}`}>
+                    <span></span>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Cards
--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,59 +1,45 @@
 import React from 'react'
 import './Skills.css'
+import Card from '../Cards/Cards'
 
 function Skills() {
   return (
     <div className="skills">
-      <div className="s-left">
+      <div className="s-info">
         <div className="s-head">
           <p className="head1">My</p>
           <p className="head2">SkillSet</p>
         </div>
-        <p className="s-body">Lorem ispum is simpley dummy text of printing of printing Lorem
+        <p className="s-body">Lorem ispum is simpley dummy text of printing of printing Lorem<br></br>
           ispum is simpley dummy text of printing</p>
       </div>
-      <div className="s-right">
-        <div className="s-bars">
-          <div className="bar">
-            <div className="info">
-              <span>HTML</span>
-            </div>
-            <div className="progress-line html">
-              <span></span>
-            </div>
-          </div>
-          <div className="bar">
-            <div className="info">
-              <span>CSS</span>
-            </div>
-            <div className="progress-line css">
-              <span></span>
-            </div>
-          </div>
-          <div className="bar">
-            <div className="info">
-              <span>jQuery</span>
-            </div>
-            <div className="progress-line jquery">
-              <span></span>
-            </div>
-          </div>
-          <div className="bar">
-            <div className="info">
-              <span>Python</span>
-            </div>
-            <div className="progress-line python">
-              <span></span>
-            </div>
-          </div>
-          <div className="bar">
-            <div className="info">
-              <span>MySQL</span>
-            </div>
-            <div className="progress-line mysql">
-              <span></span>
-            </div>
-          </div>
+      <div className="s-card">
+        <div className="card1">
+          <Card
+            header={"Frontend Developer"}
+            l1={"HTML"}
+            l2={"CSS"}
+            l3={"Javascript"}
+            l4={"React"}
+          />
+        </div>
+        <div className="card2">
+          <Card
+            header={"Backend Developer"}
+            l1={"PHP"}
+            l2={"NodeJS"}
+            l3={"Firebase"}
+            l4={"Python"}
+          />
+        </div>
+        <div className="card3">
+          <Card
+            header={"Designer"}
+            l1={"Figma"}
+            l2={"Sketch"}
+            l3={"Blender"}
+            l4={"GIMP"}
+          />
         </div>
       </div>
     </div>
